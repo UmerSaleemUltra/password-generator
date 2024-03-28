2907171ce8f137698065a19c77a4aacb96fad295
Umer Saleem <162819866+UmerSaleemUltra@users.noreply.github.com>
2024-03-28 13:48:58 -0700

    Add files via upload

---
+++ password generator.js
@@ -0,0 +1,13 @@
+function generatePassword() {
+  var length = document.getElementById("length").value;
+  var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+{}[]|;:,.<>?";
+  var password = "";
+
+  for (let i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+
+  document.getElementById("password").value = password;
+}
+
